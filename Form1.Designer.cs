ef1365b2f4b42f84a226158344406b56f387d0f2
Wolf <33Hantkun33@gmail.com>
2020-12-18 04:05:04 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace One_HandedBandit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button = new System.Windows.Forms.Button();
+            this.n1 = new System.Windows.Forms.Label();
+            this.n2 = new System.Windows.Forms.Label();
+            this.n3 = new System.Windows.Forms.Label();
+            this.rotate1 = new System.Windows.Forms.Timer(this.components);
+            this.rotate2 = new System.Windows.Forms.Timer(this.components);
+            this.rotate3 = new System.Windows.Forms.Timer(this.components);
+            this.stop1 = new System.Windows.Forms.Timer(this.components);
+            this.stop2 = new System.Windows.Forms.Timer(this.components);
+            this.stop3 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.balanceBox = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.Color.OrangeRed;
+            resources.ApplyResources(this.button, "button");
+            this.button.Name = "button";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // n1
+            // 
+            resources.ApplyResources(this.n1, "n1");
+            this.n1.BackColor = System.Drawing.Color.Firebrick;
+            this.n1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.n1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.n1.Name = "n1";
+            this.n1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // n2
+            // 
+            resources.ApplyResources(this.n2, "n2");
+            this.n2.BackColor = System.Drawing.Color.Firebrick;
+            this.n2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.n2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.n2.Name = "n2";
+            this.n2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // n3
+            // 
+            resources.ApplyResources(this.n3, "n3");
+            this.n3.BackColor = System.Drawing.Color.Firebrick;
+            this.n3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.n3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.n3.Name = "n3";
+            this.n3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // rotate1
+            // 
+            this.rotate1.Interval = 15;
+            this.rotate1.Tick += new System.EventHandler(this.rotate1_Tick);
+            // 
+            // rotate2
+            // 
+            this.rotate2.Interval = 30;
+            this.rotate2.Tick += new System.EventHandler(this.rotate2_Tick);
+            // 
+            // rotate3
+            // 
+            this.rotate3.Interval = 40;
+            this.rotate3.Tick += new System.EventHandler(this.rotate3_Tick);
+            // 
+            // stop1
+            // 
+            this.stop1.Interval = 2000;
+            this.stop1.Tick += new System.EventHandler(this.stop1_Tick);
+            // 
+            // stop2
+            // 
+            this.stop2.Interval = 3450;
+            this.stop2.Tick += new System.EventHandler(this.stop2_Tick);
+            // 
+            // stop3
+            // 
+            this.stop3.Interval = 4200;
+            this.stop3.Tick += new System.EventHandler(this.stop3_Tick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.DarkRed;
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.ForeColor = System.Drawing.Color.Black;
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // balanceBox
+            // 
+            resources.ApplyResources(this.balanceBox, "balanceBox");
+            this.balanceBox.BackColor = System.Drawing.Color.Black;
+            this.balanceBox.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.balanceBox.Name = "balanceBox";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.Controls.Add(this.balanceBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.button);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label n1;
+        private System.Windows.Forms.Label n2;
+        private System.Windows.Forms.Label n3;
+        private System.Windows.Forms.Timer rotate1;
+        private System.Windows.Forms.Timer rotate2;
+        private System.Windows.Forms.Timer rotate3;
+        private System.Windows.Forms.Timer stop1;
+        private System.Windows.Forms.Timer stop2;
+        private System.Windows.Forms.Timer stop3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label balanceBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace One_HandedBandit
+{
+    public partial class Form1 : Form
+    {
+        int balance = 100;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            balance -= 5;
+            balanceBox.Text = System.Convert.ToString(balance);
+            rotate1.Enabled = true;
+            rotate2.Enabled = true;
+            rotate3.Enabled = true;
+            stop1.Enabled = true;
+            stop2.Enabled = true;
+            stop3.Enabled = true;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rotate1_Tick(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int num = rand.Next(8);
+            n1.Text = System.Convert.ToString(num);
+        }
+
+        private void rotate2_Tick(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int num = rand.Next(8);
+            n2.Text = System.Convert.ToString(num);
+        }
+
+        private void rotate3_Tick(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int num = rand.Next(8);
+            n3.Text = System.Convert.ToString(num);
+        }
+
+        private void stop1_Tick(object sender, EventArgs e)
+        {
+            rotate1.Enabled = false;
+            stop1.Enabled = false;
+        }
+
+        private void stop2_Tick(object sender, EventArgs e)
+        {
+            rotate2.Enabled = false;
+            stop2.Enabled = false;
+        }
+
+        private void stop3_Tick(object sender, EventArgs e)
+        {
+            rotate3.Enabled = false;
+            stop3.Enabled = false;
+            if (balance < 0)
+            {
+                MessageBox.Show("GAME OVER !");
+                balance = 100;
+            }
+
+
+            if ((n1.Text == "7") && (n2.Text == "7") && (n3.Text == "7"))
+            {
+                balance += 100000;
+                balanceBox.Text = System.Convert.ToString(balance);
+                MessageBox.Show("!!!JACKPOT!!!");
+                MessageBox.Show("You won 100 000$");
+            }
+
+
+            if ((n1.Text == "6") && (n2.Text == "6") && (n3.Text == "6"))
+            {
+                balance += 10000;
+                balanceBox.Text = System.Convert.ToString(balance);
+                MessageBox.Show("You won 10000$!");
+            }
+
+
+            if ((n1.Text == "5") && (n2.Text == "5") && (n3.Text == "5"))
+            {
+                balance += 5000;
+                balanceBox.Text = System.Convert.ToString(balance);
+                MessageBox.Show("You won 5000$!");
+            }
+
+            if ((n1.Text == "4") && (n2.Text == "4") && (n3.Text == "4"))
+            {
+                balance += 1000;
+                balanceBox.Text = System.Convert.ToString(balance);
+                MessageBox.Show("You won 1000$!");
+            }
+
+            if ((n1.Text == "3") && (n2.Text == "3") && (n3.Text == "3"))
+            {
+                balance += 500;
+                balanceBox.Text = System.Convert.ToString(balance);
+                MessageBox.Show("You won 500$!");
+            }
+
+            if ((n1.Text == "2") && (n2.Text == "2") && (n3.Text == "2"))
+            {
+                balance += 100;
+                balanceBox.Text = System.Convert.ToString(balance);
+                MessageBox.Show("You won 100$!");
+            }
+
+
+            if ((n1.Text == "2") && (n2.Text == "2"))
+            {
+                if (n3.Text != "2")
+                {
+                    balance += 15;
+                    balanceBox.Text = System.Convert.ToString(balance);
+                    MessageBox.Show("You won 15$!");
+                }
+            }
+            if ((n1.Text == "2") && (n3.Text == "2"))
+            {
+                if (n2.Text != "2")
+                {
+                    balance += 15;
+                    balanceBox.Text = System.Convert.ToString(balance);
+                    MessageBox.Show("You won 15$!");
+                }
+            }
+            if ((n3.Text == "2") && (n2.Text == "2"))
+            {
+                if (n1.Text != "2")
+                {
+                    balance += 15;
+                    balanceBox.Text = System.Convert.ToString(balance);
+                    MessageBox.Show("You won 15$!");
+                }
+            }
+
+
+            if ((n1.Text == "1") && (n2.Text == "1") && (n3.Text == "1"))
+            {
+                balance += 10;
+                balanceBox.Text = System.Convert.ToString(balance);
+                MessageBox.Show("You won 10$!");
+            }
+
+            if ((n1.Text == "1") && (n2.Text == "1"))
+            {
+                if (n3.Text != "1")
+                {
+                    balance += 5;
+                    balanceBox.Text = System.Convert.ToString(balance);
+                    MessageBox.Show("You won 5$!");
+                }
+            }
+            if ((n1.Text == "1") && (n3.Text == "1"))
+            {
+                if (n2.Text != "1")
+                {
+                    balance += 5;
+                    balanceBox.Text = System.Convert.ToString(balance);
+                    MessageBox.Show("You won 5$!");
+                }
+            }
+            if ((n3.Text == "1") && (n2.Text == "1"))
+            {
+                if (n1.Text != "1")
+                {
+                    balance += 5;
+                    balanceBox.Text = System.Convert.ToString(balance);
+                    MessageBox.Show("You won 5$!");
+                }
+            }
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
